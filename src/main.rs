f753bd113bb85011783ec3ca2bd6eca6e51697e5
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-11-30 06:30:14 -0500

    some programming fun for christmas 2024

--- src/main.rs
+++ src/main.rs
@@ -160,6 +160,36 @@ fn main() {
     };
 
     println!("The {}th Fibonacci number is: {}", n, fib_number);
+
+    //Christmas song generator
+    let days = [
+        "first", "second", "third", "fourth", "fifth",
+        "sixth", "seventh", "eighth", "ninth", "tenth",
+        "eleventh", "twelfth"
+    ];
+    let gifts = [
+        "a Partridge in a Pear Tree",
+        "two Turtle Doves", 
+        "three French Hens",
+        "four Calling Birds",
+        "five Gold Rings",
+        "six Geese a-Laying",
+        "seven Swans a-Swimming",
+        "eight Maids a-Milking",
+        "nine Ladies Dancing", 
+        "ten Lords a-Leaping",
+        "eleven Pipers Piping",
+        "twelve Drummers Drumming"
+    ];
+
+    for day in 0..12 {
+        println!("On the {} day of Christmas my true love sent to me", days[day]);
+
+        for gift in (0..=day).rev() {
+            println!("{}", gifts[gift]);
+        }
+        println!();
+    }
 }
 
 // Iterative approach
