d2703c8a243cac7aded0748958d8c154933acd95
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-11-30 05:28:20 -0500

    if, else if, else clauses

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let number = 9;
+
+    if number < 5 {
+        println!("number less than 5");
+    } else if number < 10 {
+        println!("number greater than 5 and less than 10");
+    } else {
+        println!("number greater than or equal to 10");
+    }
+}
